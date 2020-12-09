45d83fcb45a5ae6b0a3d2a656c0d3307c5fb8ec5
belenbernal <beelu.bernal@gmail.com>
2020-12-09 15:14:59 -0300

    Nuevo commit

--- parImpar.js
+++ parImpar.js
@@ -1,8 +1,3 @@
-/* Crear una función expresada que al recibir un numero como 
-parametro imprima por consola “El número es par” si lo es, o 
-“El número es impar” si no lo es. En el caso que el numero fuese 0 
-mostrar un mensaje de error. Se debe incluir un if ternario en script.*/
-
 let nro =0;
 function parImpar (n){
     if(n!=0){
